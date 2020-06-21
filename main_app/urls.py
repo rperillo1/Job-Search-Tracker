b4c85646dfcf0f221e8afbefec4632e4a74eb591
Robert Perillo <perillo.robert@gmail.com>
2020-06-21 11:39:59 -0600

    update navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('applications/', views.applications_index, name='applications_index'),
+    path('accounts/login/', 'django.contrib.auth.views.login', name='login'),
+    # path('accounts/logout/', 'django.contrib.auth.views.logout', name='logout'),
 ]
