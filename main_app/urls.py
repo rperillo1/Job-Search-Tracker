a5ac4eb91aa5d6f70835c93f7a15ca26feed5a87
Robert Perillo <perillo.robert@gmail.com>
2020-06-22 15:35:45 -0600

    add interview form page

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('applications/', views.applications_index, name='applications_index'),
     path('applications/create/', views.ApplicationCreate.as_view(), name='applications_create'),
     path('applications/<int:app_id>/interview/', views.interview_index, name='interview_index'),
-    path('applications/<int:app_id>/interview/create/', views.interview_create, name='interview_create'),
+    path('applications/<int:app_id>/interview/form', views.interview_form, name='interview_form'),
+    # path('applications/<int:app_id>/interview/create/', views.interview_create, name='interview_create'),
 ]
