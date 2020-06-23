c4f84a2d0ccef0b2a8212a422ed6e6d2bf9e1416
chasewri <chasewri@gmail.com>
2020-06-23 10:17:01 -0600

    add show route, UI update

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('applications/', views.applications_index, name='applications_index'),
     path('applications/create/', views.ApplicationCreate.as_view(), name='applications_create'),
+    path('applications/<int:pk>/', views.ApplicationShow.as_view(), name='applications_show'),
     path('applications/<int:app_id>/interview/', views.interview_index, name='interview_index'),
     path('applications/<int:app_id>/interview/form', views.interview_form, name='interview_form'),
     # path('applications/<int:app_id>/interview/create/', views.interview_create, name='interview_create'),
--- main_app/views.py
+++ main_app/views.py
@@ -5,19 +5,18 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .forms import UserForm, InterviewForm
 from .models import Application, Skills, Interview
+
 # Create your views here.
 
 
 def home(request):
     user_form = UserForm()
-    return render(request, 'home.html', {
-        'user_form': user_form
-    })
+    return render(request, "home.html", {"user_form": user_form})
 
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
+    error_message = ""
+    if request.method == "POST":
         # This is how to create a 'user' form object
         # that includes the data from the browser
         form = UserCreationForm(request.POST)
@@ -26,29 +25,42 @@ def signup(request):
             user = form.save()
             # This is how we log a user in via code
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            error_message = 'Invalid sign up - try again'
+            error_message = "Invalid sign up - try again"
     # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
 
 
 def applications_index(request):
     applications = Application.objects.filter(user=request.user)
     user = request.user
- 
-    return render(request, 'applications/index.html', {
-        'applications': applications,
-        'user': user,
-    })
+
+    return render(
+        request,
+        "applications/index.html",
+        {"applications": applications, "user": user,},
+    )
+
+
+class ApplicationShow(DetailView):
+    model = Application
 
 
 class ApplicationCreate(CreateView):
     model = Application
-    fields = ['status', 'date_applied', 'title', 'company',
-              'description', 'notes', 'salary', 'interest_level']
+    fields = [
+        "status",
+        "date_applied",
+        "title",
+        "company",
+        "description",
+        "notes",
+        "salary",
+        "interest_level",
+    ]
 
     def form_valid(self, form):
         # Assign the logged in user (self.request.user)
@@ -59,14 +71,16 @@ class ApplicationCreate(CreateView):
 
 def interview_index(request, app_id):
     interviews = Interview.objects.filter(application=app_id)
-    return render(request, 'interviews/index.html', {
-        'application_id': app_id,
-        'interviews': interviews
-    })
+    return render(
+        request,
+        "interviews/index.html",
+        {"application_id": app_id, "interviews": interviews},
+    )
 
 
 def interview_form(request, app_id):
-    return render(request, 'interviews/form.html')
+    return render(request, "interviews/form.html")
+
 
 def interview_create(request, app_id):
     form = InterviewForm(request.POST)
@@ -76,5 +90,5 @@ def interview_create(request, app_id):
         new_interview = form.save(commit=False)
         new_interview.application_id = app_id
         new_interview.save()
-    return redirect('interview_index', application_id=app_id)
+    return redirect("interview_index", application_id=app_id)
 
