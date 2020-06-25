4f37b66ba8f97a3c605baa042302668ab60a6d5e
chasewri <chasewri@gmail.com>
2020-06-25 09:59:47 -0600

    production vue

--- main_app/static/main_app/js/app_vue.js
+++ main_app/static/main_app/js/app_vue.js
@@ -1,38 +1,37 @@
 function transformStr(str) {
-    return str.replace(/\s/g, '+')
+  return str.replace(/\s/g, "+");
 }
-const jobTitle = document.getElementById('vue-search').innerText
-const searchTerm = transformStr(jobTitle)
-console.log(searchTerm)
+const jobTitle = document.getElementById("vue-search").innerText;
+const searchTerm = transformStr(jobTitle);
 
 const options = {
-  url: 'https://cors-anywhere.herokuapp.com/' + 'https://jobs.github.com/positions.json?search=' + searchTerm +'&location=remote',
-  method: 'GET',
-}
-
-
+  url:
+    "https://cors-anywhere.herokuapp.com/" +
+    "https://jobs.github.com/positions.json?search=" +
+    searchTerm +
+    "&location=remote",
+  method: "GET",
+};
 
 const vueApp = new Vue({
-    delimiters: ['${','}'],
-    el: '#app',
-    data () {
-        return {
-            info: null,
-            loading: true,
-            errored: false
-        }
-    },
-    mounted () {
-        axios(options)
-        .then(response => {
-            console.log(response.headers)
-            console.log(response.config)
-            this.info = response.data.slice(0,10) 
-        })
-        .catch(error => {
-            console.log(error)
-            this.errored = true
-        })
-        .finally(() => this.loading = false)
-    }
-})
+  delimiters: ["${", "}"],
+  el: "#app",
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false,
+    };
+  },
+  mounted() {
+    axios(options)
+      .then((response) => {
+        this.info = response.data.slice(0, 10);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+});
