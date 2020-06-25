324c10f461b8aa50473e8ac6e5d744f4d80a7877
chasewri <chasewri@gmail.com>
2020-06-25 10:04:25 -0600

    who knows really

--- main_app/models.py
+++ main_app/models.py
@@ -1,25 +1,22 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+
 # user model import
 from django.contrib.auth.models import User
 
 STAGES = (
-    ('A', 'Applied'),
-    ('C', 'Contacted Back'),
-    ('R', 'Recruiter Touch'),
-    ('F', '1st Interview'),
-    ('S', '2nd Interview'),
-    ('T', '3rd Interview'),
+    ("A", "Applied"),
+    ("C", "Contacted Back"),
+    ("R", "Recruiter Touch"),
+    ("F", "1st Interview"),
+    ("S", "2nd Interview"),
+    ("T", "3rd Interview"),
 )
 
 
 class Application(models.Model):
-    status = models.CharField(
-        max_length = 1,
-        choices = STAGES,
-        default = STAGES[0][0]
-    )
+    status = models.CharField(max_length=1, choices=STAGES, default=STAGES[0][0])
     date_applied = models.DateField()
     title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
@@ -33,8 +30,7 @@ class Application(models.Model):
         return f"{self.title} at {self.company} on {self.date_applied}"
 
     def get_absolute_url(self):
-        return reverse('applications_index')
-
+        return reverse("applications_index")
 
 
 class Interview(models.Model):
@@ -43,7 +39,6 @@ class Interview(models.Model):
     questions = models.TextField()
     rating = models.IntegerField()
     application = models.ForeignKey(Application, on_delete=models.CASCADE)
-
 
 
 class Skills(models.Model):
